8deaa8d21f286b1d8cfa3a131f18133111ea1326
William M <wmooney1984@gmail.com>
2019-07-24 19:03:31 -0400

    Bug Needs to be fixed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-//TODO: STEP 1 - Import the useState hook.
+//TODO: STEP 1 - Not needed for class syntax... Import the useState hook.
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
-function App() {
+class App extends React.Component {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  state = {
+    homeScore: 0,
+    awayScore: 0
+  }
+  
+  setScoreFn = (team, points) => {
+    this.setState({ team: team+=points });
+  }
 
-  return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+  render() {
+    return (
+      <div className="container">
+        <section className="scoreboard">
+          <div className="topRow">
+            <div className="home">
+              <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+              {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+              <div className="home__score">{this.homeScore}</div>
+            </div>
+            <div className="timer">00:03</div>
+            <div className="away">
+              <h2 className="away__name">Tigers</h2>
+              <div className="away__score">{this.awayScore}</div>
+            </div>
           </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+          <BottomRow />
+        </section>
+        <section className="buttons">
+          <div className="homeButtons">
+            {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+            <button onClick={ this.setScoreFn(this.homeScore, 7) } className="homeButtons__touchdown">Home Touchdown</button>
+            <button onClick={ this.setScoreFn(this.homeScore, 3) } className="homeButtons__fieldGoal">Home Field Goal</button>
           </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
-    </div>
-  );
+          <div className="awayButtons">
+            <button onClick={ this.setScoreFn(this.awayScore, 7) } className="awayButtons__touchdown">Away Touchdown</button>
+            <button onClick={ this.setScoreFn(this.awayScore, 3) } className="awayButtons__fieldGoal">Away Field Goal</button>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
